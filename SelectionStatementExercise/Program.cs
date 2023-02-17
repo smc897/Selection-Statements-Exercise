d78f4e0f47d741e32d34c13b2f20c2369ec9a66f
ScottMC89 <scottmc592@gmail.com>
2023-02-17 13:45:53 -0600

    game stuff.

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game();
+        }
+
+        static void Game() {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput > favNumber) Console.WriteLine("too high!");
+            else {
+                if (userInput < favNumber) Console.WriteLine("too low!");
+                else Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
